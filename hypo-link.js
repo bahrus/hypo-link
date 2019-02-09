2ffe1e09234243dcef6d7067e0a0008b56f4881b
bahrus <anderson.bruce.b@gmail.com>
2019-02-09 08:38:49 -0500

    ...

--- hypo-link.js
+++ hypo-link.js
@@ -3,18 +3,68 @@ import { define } from 'xtal-latx/define.js';
 import { anchorme } from './anchorme/index.js';
 const raw_content = 'raw-content';
 const re = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-zA-Z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/;
+const truncate = 'truncate';
+const default_protocol = 'default-protocol';
+const exclude_emails = 'exclude-emails';
+const exclude_urls = 'exclude-urls';
+const exclude_ips = 'exclude-ips';
+const exclude_files = 'exclude-files';
 export class HypoLink extends XtallatX(HTMLElement) {
+    constructor() {
+        super(...arguments);
+        this._options = {};
+    }
     static get is() { return 'hypo-link'; }
     static get observedAttributes() {
-        return super.observedAttributes.concat([raw_content]);
+        return super.observedAttributes.concat([raw_content, truncate, default_protocol, exclude_emails, exclude_urls, exclude_ips, exclude_files]);
     }
-    attributeChangedCallback(name, oldVal, newVal) {
+    attributeChangedCallback(name, ov, nv) {
         switch (name) {
             case raw_content:
-                this._rawContent = newVal;
+                this._rawContent = nv;
+                break;
+            case truncate:
+                this._truncate = JSON.parse(nv);
+                this._options.truncate = this._truncate;
+                break;
+            case default_protocol:
+                this._defaultProtocol = nv;
+                this._options.defaultProtocol = nv;
+                break;
+            case exclude_emails:
+                this._excludeEmails = nv !== null;
+                this._options.emails = !this._excludeEmails;
+                break;
+            case exclude_urls:
+                this._excludeUrls = nv !== null;
+                this._options.urls = !this._excludeUrls;
+                break;
+            case exclude_ips:
+                this._excludeIps = nv !== null;
+                this._options.ips = !this._excludeIps;
+                break;
+            case exclude_files:
+                this._excludeFiles = nv !== null;
+                this._options.files = !this._excludeFiles;
                 break;
         }
         this.onPropsChange();
+    }
+    get excludeEmails() { return this._excludeEmails; }
+    set excludeEmails(nv) {
+        this.attr(exclude_emails, nv, '');
+    }
+    get excludeUrls() { return this._excludeUrls; }
+    set excludeUrls(nv) {
+        this.attr(exclude_urls, nv, '');
+    }
+    get excludeIps() { return this._excludeIps; }
+    set excludeIps(nv) {
+        this.attr(exclude_ips, nv, '');
+    }
+    get excludeFiles() { return this._excludeFiles; }
+    set excludeFiles(nv) {
+        this.attr(exclude_files, nv, '');
     }
     get rawContent() {
         return this._rawContent;
@@ -22,9 +72,37 @@ export class HypoLink extends XtallatX(HTMLElement) {
     set rawContent(val) {
         this.attr(raw_content, val);
     }
+    get truncate() {
+        return this._truncate;
+    }
+    set truncate(nv) {
+        //this._truncate = nv;
+        this.attr(truncate, JSON.stringify(nv));
+    }
+    get linkAttributes() {
+        return this._linkAttributes;
+    }
+    set linkAttributes(nv) {
+        this._linkAttributes = nv;
+        this._options.attributes = nv;
+        this.onPropsChange();
+    }
+    get exclude() { return this._exclude; }
+    set exclude(nv) {
+        this._exclude = nv;
+        this._options.exclude = nv;
+        this.onPropsChange();
+    }
+    get defaultProtocol() {
+        return this._defaultProtocol;
+    }
+    set defaultProtocol(nv) {
+        this.attr(default_protocol, nv);
+    }
     connectedCallback() {
         this._c = true;
-        this._upgradeProperties(['rawContent']);
+        this._upgradeProperties(['rawContent', truncate, 'linkAttributes', 'defaultProtocol', 'excludeEmails',
+            'excludeUrls', 'excludeIps', 'excludeFiles']);
         this.onPropsChange();
     }
     onPropsChange() {
--- hypo-link.ts
+++ hypo-link.ts
@@ -1,22 +1,78 @@
 import {XtallatX} from 'xtal-latx/xtal-latx.js';
 import {define} from 'xtal-latx/define.js';
 import {anchorme} from './anchorme/index.js';
+import {Options, AttributeFunction, AttributeObj, URLObj} from "./anchorme/util.js";
 const raw_content = 'raw-content';
 const re = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-zA-Z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/;
 
+const truncate = 'truncate';
+const default_protocol = 'default-protocol';
+const exclude_emails = 'exclude-emails';
+const exclude_urls = 'exclude-urls';
+const exclude_ips = 'exclude-ips';
+const exclude_files = 'exclude-files';
 export class HypoLink extends XtallatX(HTMLElement){
     static get is(){return 'hypo-link';}
     static get observedAttributes(){
-        return super.observedAttributes.concat([raw_content]);
+        return super.observedAttributes.concat([raw_content, truncate, default_protocol, exclude_emails, exclude_urls, exclude_ips, exclude_files]);
     }
-    attributeChangedCallback(name: string, oldVal: string, newVal: string){
+    attributeChangedCallback(name: string, ov: string, nv: string){
         switch(name){
             case raw_content:
-                this._rawContent = newVal;
-                break;            
+                this._rawContent = nv;
+                break;
+            case truncate:
+                this._truncate = JSON.parse(nv);
+                this._options.truncate = this._truncate!;
+                break; 
+            case default_protocol:
+                this._defaultProtocol = nv;
+                this._options.defaultProtocol = nv;
+                break;
+            case exclude_emails:
+                this._excludeEmails = nv !== null;
+                this._options.emails = !this._excludeEmails;
+                break;
+            case exclude_urls:
+                this._excludeUrls = nv !== null;
+                this._options.urls = !this._excludeUrls;
+                break;
+            case exclude_ips:
+                this._excludeIps = nv !== null;
+                this._options.ips = !this._excludeIps;
+                break;
+            case exclude_files:
+                this._excludeFiles = nv !== null;
+                this._options.files = !this._excludeFiles;
+                break;
         }
         this.onPropsChange();
     }
+
+    _excludeEmails: boolean | undefined;
+    get excludeEmails(){return this._excludeEmails;}
+    set excludeEmails(nv){
+        this.attr(exclude_emails, nv!, '');
+    }
+
+    _excludeUrls: boolean | undefined;
+    get excludeUrls(){return this._excludeUrls;}
+    set excludeUrls(nv){
+        this.attr(exclude_urls, nv!, '');
+    }
+
+    _excludeIps: boolean | undefined;
+    get excludeIps(){return this._excludeIps;}
+    set excludeIps(nv){
+        this.attr(exclude_ips, nv!, '');
+    }
+
+    _excludeFiles: boolean | undefined;
+    get excludeFiles(){return this._excludeFiles;}
+    set excludeFiles(nv){
+        this.attr(exclude_files, nv!, '');
+    }
+    
 
     _rawContent!: string;
     get rawContent() {
@@ -25,11 +81,47 @@ export class HypoLink extends XtallatX(HTMLElement){
     set rawContent(val) {
         this.attr(raw_content, val);
     }
+    _options: Options = {};
+    _truncate: number | [number, number] | undefined;
+    get truncate(){
+        return this._truncate;
+    }
+    set truncate(nv){
+        //this._truncate = nv;
+        this.attr(truncate, JSON.stringify(nv));
+    }
+
+    _linkAttributes: Array<AttributeObj|AttributeFunction> | undefined;
+    get linkAttributes(){
+        return this._linkAttributes;
+    }
+    set linkAttributes(nv){
+        this._linkAttributes = nv;
+        this._options.attributes = nv;
+        this.onPropsChange();
+    }
+
+    _exclude: ((url:URLObj)=>boolean) | undefined;
+    get exclude(){return this._exclude;}
+    set exclude(nv){
+        this._exclude = nv;
+        this._options.exclude = nv;
+        this.onPropsChange();
+    }
+
+    _defaultProtocol: string | undefined | null;
+    get defaultProtocol(){
+        return this._defaultProtocol;
+    }
+    set defaultProtocol(nv){
+        this.attr(default_protocol, nv!);
+    }
 
     _c! : boolean;
     connectedCallback(){
         this._c = true;
-        this._upgradeProperties(['rawContent']);
+        this._upgradeProperties(['rawContent', truncate, 'linkAttributes', 'defaultProtocol', 'excludeEmails', 
+            'excludeUrls', 'excludeIps', 'excludeFiles']);
         this.onPropsChange();
     }
 
