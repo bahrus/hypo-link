452db1fab0bf4833e1d25e995f36464443879396
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-08-24 19:36:38 -0400

    ...

--- types.d.ts
+++ types.d.ts
@@ -1,21 +1,25 @@
-export interface HypoLinkProps extends Partial<HTMLElement>{
+export interface HypoLinkProps{
     /**
      * Exclude email links.
      * @attr 'exclude-emails'
      */
-    excludeEmails?: boolean | undefined;
+    excludeEmails?: boolean;
 
     /**
      * Exclude url links.
      * @attr exclude-urls
      */
-    excludeUrls?: boolean | undefined;
+    excludeUrls?: boolean;
 
     /**
      * String to hyperlink.  Gets set from innerText initially.
      * @attr raw-content
      */
-    rawContent?: string | undefined;
+    rawContent?: string;
 
-    processedContent?: string | undefined;
+    processedContent?: string;
+}
+
+export interface HypoLinkActions{
+    processContent(self: this): {processedContent: string}
 }
